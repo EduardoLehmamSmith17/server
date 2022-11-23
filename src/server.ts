f6c73f409e64aeaa5a7603f0d914893918971e4c
EduardoLehmamSmith17 <smithlehmam@gmail.com>
2022-11-22 21:50:11 -0300

    Atualization

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,40 @@
 import Fastify from 'fastify';
+
 import cors from '@fastify/cors';
 import jwt from '@fastify/jwt';
+
+import { guessRouters } from './routes/guess';
 import { poolRouters } from './routes/pool';
+import { userRouters } from './routes/user';
 import { authRouters } from './routes/auth';
 import { gameRouters } from './routes/game';
-import { guessRouters } from './routes/guess';
-import { userRouters } from './routes/user';
 
-async function bootstrap() {
+const start = async () => {
     const fastify = Fastify({
         logger: true,
     })
 
-    await fastify.register(cors, {
-        origin: true,
-    })
+    try {
+        await fastify.register(cors, {
+            origin: true,
+        })
+    
+        await fastify.register(jwt, {
+            secret: "nlwcopa",
+        })
+    
+        await fastify.register(poolRouters)
+        await fastify.register(authRouters)
+        await fastify.register(gameRouters)
+        await fastify.register(userRouters)
+        await fastify.register(guessRouters)
+    
+        await fastify.listen({ port: 3333, host: '0.0.0.0' })
+    } catch(err) {
+        fastify.log.error(err)
 
-    await fastify.register(jwt, {
-        secret: "nlwcopa",
-    })
-
-    await fastify.register(poolRouters)
-    await fastify.register(authRouters)
-    await fastify.register(gameRouters)
-    await fastify.register(userRouters)
-    await fastify.register(guessRouters)
-
-    await fastify.listen({ port: 3333, /*shost: '0.0.0.0'*/ })
-
+        process.exit(1)
+    }
 }
 
-bootstrap()
+start()
